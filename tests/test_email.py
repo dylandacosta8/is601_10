5e7b214e7e99fa9d0785dcbd7f6ccbaac6e8ec01
Dylan Da Costa <dylan2222@gmail.com>
2024-11-09 18:48:23 -0500

    minor cleanup

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,8 +1,5 @@
 import pytest
-from app.services.email_service import EmailService
-from app.utils.template_manager import TemplateManager
 
-    
 @pytest.mark.asyncio
 async def test_send_markdown_email(email_service):
     user_data = {
